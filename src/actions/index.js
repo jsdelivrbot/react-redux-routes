aca1b3f23841eba3cea28607ea9354460e774ed4
Wishmaker888 <skywalker855@gmail.com>
2017-07-06 14:20:57 +0300

    Continue project..

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=WISHMAKER87';
+const API_KEY = '?key=wishmaker87';
 
 export function fetchPosts() {
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import promise from 'redux-promise';
 
-import PostsIndex from './components/posts_index';
-
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,6 +16,7 @@ ReactDOM.render(
 		<BrowserRouter>
 			<div>
 				<Route path="/" component={PostsIndex} />
+				<Route path="/posts/new" component={PostsNew} />
 			</div>
 		</BrowserRouter>
   </Provider>
